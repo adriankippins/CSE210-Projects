a51ca21bb8b76d4388a6df9cf4d4c86599981ceb
Roberto Kippins <robertokippins@iotgy.net>
2023-01-14 23:54:16 -0400

    Updated Guessign Code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First name?: ");
+        string firstName = Console.ReadLine();
+        
+        Console.Write("What is your Last name?: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"The name is {lastName}, {firstName} {lastName}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentText = Console.ReadLine();
+        int gradePercent = int.Parse(gradePercentText);
+
+        string letter = "";
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations, You have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, You did not pass the course. Better luck next time.");
+        }
+
     }
 }
